96cb94d035850f91c61a2c9204f809191c32ba28
enioandrade <enio.goulart@gmail.com>
2022-05-30 19:26:20 -0300

    feat: implementing user service. methods: [singIn, singUp]

---
+++ back-end/src/app/services/user-service.js
@@ -0,0 +1,36 @@
+const bcryptjs = require('bcryptjs');
+const jwt = require('../utils/jwt');
+const userModel = require('../models/user-model');
+const errorUtil = require('../utils/error-util');
+
+module.exports = {
+  async singIn(login) {
+    const user = await userModel.findByEmail(login.email);
+    if (!user) errorUtil('Not found user', 'unauthorized');
+
+    if (!bcryptjs.compareSync(login.password, user.password)) {
+      errorUtil('Incorret password', 'unauthorized');
+    }
+
+    delete user.createAt;
+    delete user.updateAt;
+    const token = jwt.generateToken(user);
+
+    delete user.password;
+
+    return { ...user, token };
+  },
+
+  async singUp(user) {
+    const foundUser = await userModel.findByEmail(user.email);
+    if (foundUser) errorUtil('User exists', 'bad_request');
+
+    const createdUser = await userModel.create(user);
+    delete createdUser.createAt;
+    delete createdUser.updateAt;
+    const token = jwt.generateToken(createdUser);
+    delete createdUser.password;
+
+    return { ...createdUser, token };
+  },
+};
