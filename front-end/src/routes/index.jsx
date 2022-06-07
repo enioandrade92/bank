4064b43f20ba50cd5bfda3323828c54ddce5624f
enioandrade <enio.goulart@gmail.com>
2022-06-07 20:44:18 -0300

    feat: implementing the routes

---
+++ front-end/src/routes/index.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
+
+export default function Router() {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <SignIn />}/>
+        <Route path='/signup' element={ <SignUp />}/>
+        <Route path='/dashboard' element={ <Dashboard />}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
