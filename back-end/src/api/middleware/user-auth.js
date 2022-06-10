0916a531d329dabc2382a4baf1e173b4736e8a4e
enioandrade <enio.goulart@gmail.com>
2022-06-10 13:53:23 -0300

    refactor: implement function that generates an error

--- back-end/src/api/middleware/user-auth.js
+++ back-end/src/api/middleware/user-auth.js
@@ -1,13 +1,12 @@
 const jwt = require('../../app/utils/jwt');
+const errorUtil = require('../../app/utils/error-util');
 
-module.exports = (req, res, next) => {
+module.exports = (req, _res, next) => {
   try {
     const payload = jwt.verifyToken(req.headers.authorization);
     req.user = payload;
     next();
   } catch (error) {
-    const err = new Error('Token invalid or expired');
-    err.name = 'unauthorized';
-    throw err;
+    errorUtil('Token invalid or expired', 'unauthorized');
   }
 };
