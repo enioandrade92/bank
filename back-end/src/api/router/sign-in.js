5f9e479fd96a0506b71b3ef6dde89f218e560f1b
enioandrade <enio.goulart@gmail.com>
2022-06-10 13:50:08 -0300

    remove: routes signin & signup

--- back-end/src/api/router/sign-in.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const userController = require('../../app/controllers/user-controller');
-
-const signinRouter = Router();
-
-signinRouter.post('/', userController.signIn);
-
-module.exports = signinRouter;
--- back-end/src/api/router/sign-up.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const user = require('../../app/controllers/user-controller');
-
-const signupRouter = Router();
-
-signupRouter.post('/', user.signUp);
-
-module.exports = signupRouter;
