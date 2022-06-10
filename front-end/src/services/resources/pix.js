ea1920d2672da77ca2a85253b8de69752db81f0e
enioandrade <enio.goulart@gmail.com>
2022-06-10 14:15:53 -0300

    feat: implements methods request, pay, transactions

--- front-end/src/services/resources/pix.js
+++ front-end/src/services/resources/pix.js
@@ -1,7 +1,34 @@
 import api from '../api';
 
-export const request = async (value) => api.post('/pix/request', {value});
+export const request = async (value) => {
+  try {
+    const { data } = await api.post('/pix/request', {value});
+    return data;
+    
+  } catch (error) {
+    console.log(error);
+    return error.response.data;
+  }
+};
 
-export const pay = async (key) => api.post(`/pix/pay/${key}`);
+export const pay = async (key) => {
+  try {
+    const { data } = await api.post(`/pix/pay/${key}`);
+    return data;
+    
+  } catch (error) {
+    console.log(error);
+    return error.response.data;
+  }
+};
 
-export const transactions = async () => api.get('/pix/transactions');
+export const transactions = async () => {
+  try {
+    const { data } = await api.get('/pix/transactions');
+    return data;
+    
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
