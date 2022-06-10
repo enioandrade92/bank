b4b4d3cb7257f653ab5358609c4e1e9e20cf4287
enioandrade <enio.goulart@gmail.com>
2022-06-10 14:14:53 -0300

    feat: add axios interceptors

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -2,7 +2,19 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://localhost:3001',
-  timeout: 5000,
+  timeout: 4000,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('@Bank:Token') || '';
+  const configuration = config
+  
+  configuration.headers = {
+    'Authorization': token,
+  }
+
+  return configuration;
+
 })
 
 export default api;
