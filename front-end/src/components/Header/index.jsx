0cf85d34b79c8536920cfa8148bc13ad84455070
enioandrade <enio.goulart@gmail.com>
2022-06-10 14:09:22 -0300

    rafactor: Header Component

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -1,26 +1,36 @@
 import { useNavigate } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 import { HeaderContainer, HeaderWrapper, UserInfo } from './styles';
 import UserCircle from '../UserCircle';
 import logoBank from '../../assets/images/logoBank.jpg';
 
 export default function Header(){
   const navigate = useNavigate();
+  const { user } = useAuth();
+
   const handleLogOut = () => {
+    localStorage.setItem('@Bank:Token', '')
     navigate('/');
   }
-  const initials = 'EA'
+
+  if (!user.firstName) handleLogOut();
+  const initials = user.firstName.substr(0,1) + user.lastName.substr(0,1);
+
   return (
     <HeaderContainer>
       <HeaderWrapper>
-        <img src={logoBank} width={172} height={61} alt='logo bank' />
+        <a href="#" onClick={handleLogOut}>
+          <img src={logoBank} width={172} height={61} alt='logo bank' />
+        </a>
         <UserInfo>
-          <UserCircle initials={initials}/>
+          <UserCircle initials={initials || handleLogOut}/>
           <div>
-            <p> Olá. <span>Enio</span></p>
+            <p> Olá. <span><strong>{user.firstName} {user.lastName}</strong></span></p>
+            <p>{user.accountNumber}-{user.accountDigit}</p>
             <a href="#" onClick={handleLogOut}>Sair</a>
           </div>
         </UserInfo>
       </HeaderWrapper>
-    </HeaderContainer>
+    </HeaderContainer> 
   )
 };
